14e1f943edc5a0e1a1ad8f3b03012f05b4623b1e
ntedja <220712036@students.uajy.ac.id>
2025-03-01 17:46:00 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gdlibrary_d_2036
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/gdlibrary_d_2036/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gdlibrary_d_2036
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
